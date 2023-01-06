270ebb3c0c8dd8093c2a7fdbb5aee529ffd22586
Le Kang Wang <lekang.wang@yahoo.ca>
2023-01-05 20:44:34 -0500

    383e_ransom_note

---
+++ leetcode/hashtable/383e_ransom_note.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/ransom-note/
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ht = {}
+        total = len(magazine)
+        for ch in magazine:
+            if ht.get(ch) == None:
+                ht[ch] = 1
+            else:
+                ht[ch] += 1
+
+        for ch in ransomNote:
+            if ht.get(ch) == None or ht.get(ch) - 1 < 0: 
+                return False 
+            else:
+                ht[ch] -= 1
+                total -= 1
+            
+        return True
+
+        
